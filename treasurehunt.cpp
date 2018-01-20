cdb6323a9ec554d9071e14e261cd7d6f421f8a7b
Lynn <32795221+linnnruoo@users.noreply.github.com>
2018-01-20 17:25:25 +0800

    Add files via upload

---
+++ treasurehunt.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+#define ROW 200
+#define COL 200
+
+void scanarr(int row, int col, char map[][COL]){
+	for (int i=0; i<row; i++){
+		for(int j=0; j<col; j++){
+			cin>>map[i][j];
+		}
+	}
+}
+
+void go(int row, int col, char map[][COL], int numMoves, int movRow, int movCol){
+
+	if(movRow<0 || movCol<0 || movRow>=row || movCol>=col){
+		cout<<"Out"<<endl;
+	}else if(numMoves>=row*col){
+		cout<<"Lost"<<endl;
+
+	}else{
+		switch(map[movRow][movCol]){
+			case 'N': go(row, col, map, numMoves+1, movRow-1, movCol); break;
+			case 'S': go(row, col, map, numMoves+1, movRow+1, movCol); break;
+			case 'W': go(row, col, map, numMoves+1, movRow, movCol-1); break;
+			case 'E': go(row, col, map, numMoves+1, movRow, movCol+1); break;
+			case 'T': cout<<numMoves<<endl;
+		}
+	}
+
+}
+
+int main() {
+    char map[ROW][COL];
+    int row, col;
+    int numMoves=0, movRow=0, movCol=0;
+
+    cin>>row>>col;
+    scanarr(row, col, map);
+    go(row, col, map, numMoves, movRow, movCol);
+
+    return 0;
+}
