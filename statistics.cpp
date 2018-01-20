052873a3d4a280fc7a59228735ce34ff137ee2c0
Lynn <32795221+linnnruoo@users.noreply.github.com>
2018-01-20 14:39:19 +0800

    Update statistics.cpp

--- statistics.cpp
+++ statistics.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main(){
 	int count=0, max, min, num, x;
 
-	while(cin>>num){
-		int isNewCase = 1;
-        
+	while(cin>>num){                    //the while loop breaks if it reaches eof
+		int isNewCase = 1;          //flag
+                                            //num is the first integer in the new line, 
+		                            //indicating how many numbers are there in the new case
 		for(int i=0; i<num; i++) {
-			cin>>x;
-			if(x>max||isNewCase==1) max = x;
+			cin>>x;             //x will be the integers scanned 
+			if(x>max||isNewCase==1) max = x;  //isNewCase==1 to makesure that max and min are initialised
 			if(x<min||isNewCase==1) min = x;
 			isNewCase = 0;
 		}
